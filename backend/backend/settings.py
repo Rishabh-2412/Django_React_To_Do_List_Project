109a4e143362e54b48734a6f07982ae2a9d65f6a
Rishabh-2412 <727724euit196@skcet.ac.in>
2026-07-07 22:59:46 +0530

    Initial React+Django Collaboration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,10 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
@@ -123,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
